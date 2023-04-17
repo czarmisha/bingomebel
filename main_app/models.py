8a3512cba875c06eebd42f08765c1d7d15be5f28
mihail <m.dergachyov@gmail.com>
2023-04-17 15:50:07 -0400

    asdasd

--- main_app/models.py
+++ main_app/models.py
@@ -2,4 +2,15 @@ from django.db import models
 
 
 class KitchenRequest(models.Model):
-    
+    phone = models.CharField(max_length=20, verbose_name='Телефон')
+    type = models.CharField(max_length=50, verbose_name='Планировка')
+    size = models.CharField(max_length=50, verbose_name='Размер')
+    style = models.CharField(max_length=50, verbose_name='Стиль')
+    fasad = models.CharField(max_length=50, verbose_name='Материал фасада')
+    stoleshnica = models.CharField(max_length=50, verbose_name='Материал столешницы')
+    budget = models.CharField(max_length=50, verbose_name='Бюджет')
+    name = models.CharField(max_length=50, verbose_name='Имя')
+    phone = models.CharField(max_length=50, verbose_name='phone')
+
+    def __str__(self) -> str:
+        return self.name + self.phone
