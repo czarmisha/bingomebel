9fd16f432431abcd1f98d7a79603b029df722813
Mihail <vertigomisha@gmail.com>
2023-04-20 12:45:23 +0500

    asdas

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from main_app import views
 
@@ -7,4 +8,7 @@ app_name = 'main_app'
 urlpatterns = [
     path('', views.home, name='home'),
     path('kitchen', views.kitchen_form, name='kitchen_form'),
+]
+urlpatterns += [
+    path('robots.txt', TemplateView.as_view(template_name="bingo/robots.txt", content_type='text/plain')),
 ]
